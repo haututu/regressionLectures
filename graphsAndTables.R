2529d39e1bc7896aec42943ed16b2b634819f904
Taylor <taylor.winter@vuw.ac.nz>
2020-01-08 22:38:45 +1300

    Moved files into subfolders and added powerpoint and outline.

--- graphsAndTables.R
+++ graphsAndTables.R
@@ -63,3 +63,53 @@ regression_data %>%
 
 summary(lm("height ~ sex + weight", regression_data))$coefficients %>%
   round(., 4)
+
+# Interaction between continous and categorical variable
+summary(lm("k10 ~ age * nzdep", nzhs))$coefficients %>%
+  round(., 4)
+
+nzhs %>%
+  ggplot(aes(x = age, y = k10, group=sex, color=sex)) +
+  geom_smooth(method = "lm", se=FALSE, aes(linetype=sex)) +
+  theme_classic() +
+  labs(
+    x="Age (years)",
+    y="Stress",
+    color="Sex",
+    linetype="Sex"
+  ) +
+  theme(
+    text = element_text(size = 20)
+  )
+
+# Interaction between continuous variables
+interaction_data <- readRDS("../../../socialMediaMood/dataForBen.RDS") %>%
+  select(id, day, sn, fomo, negative) %>%
+  group_by(id, day) %>%
+  summarise_all(mean) %>%
+  ungroup() %>%
+  mutate(fomo_group = case_when(
+    fomo < -1 ~ "Low (-1SD)",
+    fomo > 1 ~ "High (+1SD)",
+    TRUE ~ "Average (0SD)"
+    ),
+    fomo_group = ordered(fomo_group, levels=c("High (+1SD)", "Average (0SD)", "Low (-1SD)"))
+    )
+
+interaction_data %>%
+  ggplot(aes(x=sn, y=negative, group=fomo_group, color=fomo_group)) +
+  geom_smooth(method = "lm", se=FALSE, aes(linetype=fomo_group)) +
+  theme_classic() +
+  labs(
+    x="Social Network Use",
+    y="Negative mood",
+    color="FoMO",
+    linetype="FoMO"
+  ) +
+  theme(
+    text = element_text(size = 20)
+  ) +
+  scale_color_discrete()
+
+summary(lm("negative ~ sn * fomo", interaction_data))$coefficients %>%
+  round(., 4)
